d2eefd44be64b5e5a247890f18ea511515e52430
Torbjørn Antonsen <torbjant@ntnu.no>
2026-02-25 10:42:21 +0100

    fixed program for controlling hubs

--- base_hub.py
+++ base_hub.py
@@ -1,55 +1,84 @@
 from pybricks.pupdevices import Motor
-from pybricks.parameters import Port
+from pybricks.parameters import Direction, Port
 from pybricks.tools import wait
+from pybricks.parameters import Color
+from pybricks.hubs import TechnicHub
+
+
+# Standard MicroPython modules
 from usys import stdin, stdout
 from uselect import poll
 
-# --- Initialize motors ---
-left_motor = Motor(Port.C)
-right_motor = Motor(Port.D)
-rotate_motor = Motor(Port.B)
 
-# --- Setup stdin polling ---
-keyboard = poll()
-keyboard.register(stdin)
+
+hub = TechnicHub()
+
+# hue = 0
+# saturation = 100
+# brightness = 100
+
+# ---- Initialize Motors ----
+left_motor = Motor(Port.C, positive_direction=Direction.CLOCKWISE)
+right_motor = Motor(Port.D)
+rotation_motor = Motor(Port.B)
+
+def convert_byte_to_duty_cycle(byte):
+    return ((byte / 127) - 1) * 100
 
 while True:
-    # Tell PC we are ready
+
+    # Let the remote program know we are ready for a command.
     stdout.buffer.write(b"rdy")
 
-    # Wait until some data arrives
-    while not keyboard.poll(0):
-        wait(10)
 
-    # Read 3 bytes: one for each motor
-    cmd = stdin.buffer.read(3)
-    if not cmd or len(cmd) != 3:
-        continue  # skip if incomplete
+    # Read three bytes.
+    cmd = stdin.buffer.read(4)
 
-    # Each byte controls a motor: 0 = stop, 1 = forward, 2 = backward
-    # Left motor
-    if cmd[0] == 0x01:
-        left_motor.run_angle(50, 360)
-    elif cmd[0] == 0x02:
-        left_motor.dc(50, -360 )
-    else:
-        left_motor.stop()
+    if not cmd:
+        continue
+    # hub.light.on(Color(hue, brightness, saturation))
+    #
+    # # Decide what to do based on the command.
+    # if cmd[0] == 0x01:
+    #     hue += 1
+    #     if hue > 360:
+    #         hue = 0
+    # elif cmd[0] == 0x02:
+    #     hue -= 1
+    #     if hue < 0:
+    #         hue = 360
+    #
+    # if cmd[1] == 0x01:
+    #     saturation += 1
+    #     if saturation > 100:
+    #         saturation = 100
+    # elif cmd[1] == 0x02:
+    #     saturation -= 1
+    #     if saturation < 0:
+    #         saturation = 0
+    #
+    # if cmd[2] == 0x01:
+    #     brightness += 1
+    #     if brightness > 100:
+    #         brightness = 100
+    # elif cmd[2] == 0x02:
+    #     brightness -= 1
+    #     if brightness < 0:
+    #         brightness = 0
 
-    # Right motor
-    if cmd[1] == 0x01:
-        right_motor.dc(50, 360)
-    elif cmd[1] == 0x02:
-        right_motor.dc(50, -360)
-    else:
-        right_motor.stop()
+    left_speed = convert_byte_to_duty_cycle(cmd[0])
+    right_speed = convert_byte_to_duty_cycle(cmd[1])
+    rotation_speed = convert_byte_to_duty_cycle(cmd[2])
 
-    # Rotate motor
-    if cmd[2] == 0x01:
-        rotate_motor.dc(50)
-    elif cmd[2] == 0x02:
-        rotate_motor.dc(-50)
-    else:
-        rotate_motor.stop()
+    # Apply speeds
+    left_motor.dc(left_speed)
+    right_motor.dc(right_speed)
+    rotation_motor.dc(rotation_speed)
 
-    # Small delay to prevent busy loop
-    wait(20)
+    if cmd[3] == 0x01:
+        break
+
+# Stop motors when exiting
+left_motor.stop()
+right_motor.stop()
+rotation_motor.stop()
--- crane_hub.py
+++ crane_hub.py
@@ -1,19 +1,84 @@
-from pybricks.hubs import TechnicHub
 from pybricks.pupdevices import Motor
 from pybricks.parameters import Port
 from pybricks.tools import wait
-from pybricks.bluetooth import BLEUART
+from pybricks.parameters import Color
+from pybricks.hubs import TechnicHub
+
+
+# Standard MicroPython modules
+from usys import stdin, stdout
+from uselect import poll
+
+
 
 hub = TechnicHub()
-boom = Motor(Port.A)
-jib = Motor(Port.B)
-hoist = Motor(Port.D)
-uart = BLEUART()
+
+# hue = 0
+# saturation = 100
+# brightness = 100
+
+# ---- Initialize Motors ----
+boom_motor = Motor(Port.A)
+jib_motor = Motor(Port.D)
+hoist_motor = Motor(Port.B)
+
+def convert_byte_to_duty_cycle(byte):
+    return ((byte / 127) - 1) * 100
 
 while True:
-    if uart.any():
-        data = uart.read(3)
-        boom.run((data[0] - 127) * 5)
-        jib.run((data[1] - 127) * 5)
-        hoist.run((data[2] - 127) * 5)
-    wait(20)
+
+    # Let the remote program know we are ready for a command.
+    stdout.buffer.write(b"rdy")
+
+
+    # Read three bytes.
+    cmd = stdin.buffer.read(4)
+
+    if not cmd:
+        continue
+    # hub.light.on(Color(hue, brightness, saturation))
+    #
+    # # Decide what to do based on the command.
+    # if cmd[0] == 0x01:
+    #     hue += 1
+    #     if hue > 360:
+    #         hue = 0
+    # elif cmd[0] == 0x02:
+    #     hue -= 1
+    #     if hue < 0:
+    #         hue = 360
+    #
+    # if cmd[1] == 0x01:
+    #     saturation += 1
+    #     if saturation > 100:
+    #         saturation = 100
+    # elif cmd[1] == 0x02:
+    #     saturation -= 1
+    #     if saturation < 0:
+    #         saturation = 0
+    #
+    # if cmd[2] == 0x01:
+    #     brightness += 1
+    #     if brightness > 100:
+    #         brightness = 100
+    # elif cmd[2] == 0x02:
+    #     brightness -= 1
+    #     if brightness < 0:
+    #         brightness = 0
+
+    boom_speed = convert_byte_to_duty_cycle(cmd[0])
+    jib_speed = convert_byte_to_duty_cycle(cmd[1])
+    hoist_speed = convert_byte_to_duty_cycle(cmd[2])
+
+    # Apply speeds
+    boom_motor.dc(boom_speed)
+    jib_motor.dc(jib_speed)
+    hoist_motor.dc(hoist_speed)
+
+    if cmd[3] == 0x01:
+        break
+
+# Stop motors when exiting
+boom_motor.stop()
+jib_motor.stop()
+hoist_motor.stop()
